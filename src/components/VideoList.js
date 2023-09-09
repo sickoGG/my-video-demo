ad0cd16dd7f0278d6e2e66ebb8ca3346c4896ba1
sickoGG <sicksickch@gmail.com>
2023-09-10 00:08:04 +0700

    full page no css

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import videoData from "../videodata.json";
 import {Link} from "react-router-dom";
-
 function VideoList() {
     return (
         <div className="VideoList">
         <header className="VideoList-header">
             <h1>Video List</h1>
-            <Link to="/video/1">Video 1</Link>
         </header>
+            <ul>
+                {videoData.map((video) => (
+                    <li key={video.id}>
+                        <Link to={`/video/${video.id}`}>
+                            <img src={"../../favicon.ico"} alt={video.title} />
+                            <p>{video.title}</p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
-
+import {Link, useParams} from "react-router-dom";
+import videoData from "../videodata.json";
 function VideoPlayer() {
     const { id } = useParams();
+    const video = videoData.find((v) => v.id === parseInt(id));
     return (
-        <div className="VideoPlayer">
-        <header className="VideoPlayer-header">
-         <h1>Video Player</h1>
-        </header>
-        <p>Video ID: {id}</p>
-        </div>
+
+    <div className="video-player">
+        <h2>{video.title}</h2>
+        <video width="640" height="360" controls>
+            <source src={`/videos/${video.videoUrl}`} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+        <Link to="/">Back to Video List</Link>
+    </div>
     );
 }
 export default VideoPlayer;
