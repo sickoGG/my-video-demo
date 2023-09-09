1197de874f5a39848b8f7cbe36d379da1229cf9d
sickoGG <sicksickch@gmail.com>
2023-09-09 23:19:53 +0700

    ไฟล์videodata

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 function VideoPlayer() {
+    const { id } = useParams();
     return (
         <div className="VideoPlayer">
         <header className="VideoPlayer-header">
          <h1>Video Player</h1>
         </header>
+        <p>Video ID: {id}</p>
         </div>
     );
 }
