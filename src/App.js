6e854802364c76fdf32536707616982454a8488e
sickoGG <sicksickch@gmail.com>
2023-09-10 15:45:20 +0700

    add bootstrap 5

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import VideoList from './components/VideoList';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import VideoPlayer from "./components/VideoPlayer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
-        <div className="app">
+        <div className="App">
             <Router>
                 <Routes>
                     <Route path="/" element={<VideoList />} />
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -7,16 +7,22 @@ function VideoList() {
         <header className="VideoList-header">
             <h1>Video List</h1>
         </header>
-            <ul>
-                {videoData.map((video) => (
-                    <li key={video.id}>
-                        <Link to={`/video/${video.id}`}>
-                            <img src={"../../favicon.ico"} alt={video.title} />
-                            <p>{video.title}</p>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div className="VideoList-intro">
+                <div className="container">
+                    <div className="row">
+                        {videoData.map((video) => (
+                        <div className="videoList-body col-md-4 mb-2" key={video.id}>
+                            <Link to={`/video/${video.id}`}>
+                                <img src={"../../favicon.ico"} alt={video.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{video.title}</h5>
+                                </div>
+                            </Link>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
